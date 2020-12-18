d7a96c2681c362fa58777b17d874170afaad4fbd
Edouard Lopez <contact@edouard-lopez.com>
2020-12-18 09:21:16 +0100

    fix frontend App.test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Home link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const menuElement = screen.getByText(/Home/i);
+  expect(menuElement).toBeInTheDocument();
 });
