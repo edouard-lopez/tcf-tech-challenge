3f78bf3d5264f43f8648775906b77cef376dec7f
Edouard Lopez <contact@edouard-lopez.com>
2020-12-18 09:54:28 +0100

    add a bit of love with Bulma

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,51 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
 import Categories from "./Categories";
 import Questions from "./Questions";
-
-function Home() {
-  return <h1>TCF</h1>;
-}
+import "bulma";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/categories">Categories</Link>
-            </li>
-            <li>
-              <Link to="/questions/2">Questions</Link>
-            </li>
-          </ul>
-        </nav>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              TCF
+            </Link>
+            <a
+              role="button"
+              className="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
 
-        <Route path="/" exact component={Home} />
-        <Route path="/categories" exact component={Categories} />
-        <Route path="/questions/:id" component={Questions} />
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/">
+                Home
+              </Link>
+              <Link className="navbar-item" to="/categories">
+                Categories
+              </Link>
+              <Link className="navbar-item" to="/questions/2">
+                Questions
+              </Link>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <Route path="/" exact component={Home} />
+          <Route path="/categories" exact component={Categories} />
+          <Route path="/questions/:id" component={Questions} />
+        </div>
       </div>
     </Router>
   );
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+const Home = () => {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title">The ChatBot Factory</h1>
+          <h2 className="subtitle">Challenge</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
